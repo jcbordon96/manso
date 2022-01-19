8c3a56f7418d64da8e9d1c54f2a9cd554c7dff7d
jcbordon96 <jcbordon96@gmail.com>
2022-01-19 12:56:34 -0300

    Change device

--- gps_test.py
+++ gps_test.py
@@ -16,33 +16,31 @@ class Run():
     def __init__(self):
         self.gnss_pub = rospy.Publisher("gnss", NavSatFix, queue_size=50)
         self.time_pub = rospy.Publisher("gnss_time", String, queue_size=50)
-        while True:
-            # try:
-            coords = gps.geo_coords()
-            time.data = str(coords.year).zfill(2)+str(coords.month).zfill(2)+str(coords.day).zfill(2)
-            gnss.header.stamp = rospy.Time.now()
-            gnss.latitude = round(coords.lat,7)
-            gnss.longitude = round(coords.lon,7)
-            gnss.altitude = coords.height
-            self.gnss_pub.publish(gnss)
-            time.data = str(coords.year).zfill(2)+str(coords.month).zfill(2)+str(coords.day).zfill(2)+"_"+str(coords.hour).zfill(2)+str(coords.min).zfill(2)+str(coords.sec).zfill(2)
-            self.time_pub.publish(time)
+        try:
+            print("Listenting for UBX Messages.")
+            while True:
+                try:
+                    coords = gps.geo_coords()
+                    time.data = str(coords.year).zfill(2)+str(coords.month).zfill(2)+str(coords.day).zfill(2)
+                    gnss.header.stamp = rospy.Time.now()
+                    gnss.latitude = round(coords.lat,7)
+                    gnss.longitude = round(coords.lon,7)
+                    gnss.altitude = coords.height
+                    self.gnss_pub.publish(gnss)
+                    time.data = str(coords.year).zfill(2)+str(coords.month).zfill(2)+str(coords.day).zfill(2)+"_"+str(coords.hour).zfill(2)+str(coords.min).zfill(2)+str(coords.sec).zfill(2)
+                    self.time_pub.publish(time)
 
-            print(str(coords.year).zfill(2)+str(coords.month).zfill(2)+str(coords.day).zfill(2)+"_"+str(coords.hour).zfill(2)+str(coords.min).zfill(2)+str(coords.sec).zfill(2)+"_"+str(round(coords.lon,7))+"_"+str(round(coords.lat,7))+"_"+"x")
-                # print(coords.lon, coords.lat)
-            # except (ValueError, IOError) as err:
-            #     print(err)
-            # except:
-            #     print("Some")
-            #     break
+                    print(str(coords.year).zfill(2)+str(coords.month).zfill(2)+str(coords.day).zfill(2)+"_"+str(coords.hour).zfill(2)+str(coords.min).zfill(2)+str(coords.sec).zfill(2)+"_"+str(round(coords.lon,7))+"_"+str(round(coords.lat,7))+"_"+"x")
+                    # print(coords.lon, coords.lat)
+                except (ValueError, IOError) as err:
+                    print(err)
 
-        port.close()
+        finally:
+            port.close()
 
 
 if __name__ == '__main__':
     rospy.init_node('GNSS')
-    try:
-        Run()
-    except KeyboardInterrupt:
-        print("chau")
+    
+    Run()
     rospy.spin()
